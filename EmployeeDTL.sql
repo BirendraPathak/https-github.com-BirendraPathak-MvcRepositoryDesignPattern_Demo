ab10648e68f9796c55360520b981cef3ed48cab4
BirendraPathak <pathak.birendra@gmail.com>
2024-03-12 14:55:41 +0530

    Add files via upload

---
+++ EmployeeDTL.sql
@@ -0,0 +1,15 @@
+create database EmployeeDTL
+
+CREATE TABLE Employee
+(
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](500) NULL,
+	[MiddleName] [varchar](500) NULL,
+	[LastName] [varchar](500) NULL
+)
+
+select * from Employee
+
+
+
+
